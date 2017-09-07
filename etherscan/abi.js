197b9e5021a378bedc0651adc2db1a0e81d18bb6
Anton Egorov <anton.egoroff@gmail.com>
2017-09-07 18:46:53 +0300

    latest voting abi

--- etherscan/abi.js
+++ etherscan/abi.js
@@ -3,9 +3,9 @@ var abi = require('ethereumjs-abi')
 var parameterTypes = ['uint8', 'address', 'uint', 'uint']
 var parameterValues = [
   '2',
-  '0x1b10A9D3517b29CCF8b39320Cd94468dA049a927',
+  '0x68AA3F232dA9bdC2343465545794ef3eEa5209BD',
   '100000000000000000000000',
-  '1575657'
+  '4272272'
 ]
 
 var encoded = abi.rawEncode(parameterTypes, parameterValues)
